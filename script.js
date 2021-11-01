ad925e83ba01e278eb85d036ef4e7e33e9c9eec1
fatihyanik <fatihyanik07@gmail.com>
2021-11-01 15:57:05 +0100

    I added local storage

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
 
 container.addEventListener('click', function(e){
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
@@ -17,8 +18,33 @@ select.addEventListener('change', function(e){
 });
 
 function calculateTotal(){
-    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-    let price = select.value;
+    const selectedSeats = container.querySelectorAll('.seat.selected')
+    let selectedSeatCount = selectedSeats.length;
+
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArr.push(seat);
+    });
+
+    // burada spread kullanilabilir
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+    //[1,3,5]
+    let selectedSeatIndex = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+    
     count.innerText = selectedSeatCount;
-    amount.innerText = selectedSeatCount * price;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocalStorage(selectedSeatIndex);
+}
+
+function saveToLocalStorage(index){
+    localStorage.setItem('selectedSeats', JSON.stringify(index));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
 }
